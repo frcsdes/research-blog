0f5dce2cce2c032a89a2ca2f862525e1d3e130bc
François Desrichard <git@desrichard.fr>
2018-12-29 04:15:46 +0100

    Correctly rendering nested components

--- index.js
+++ index.js
@@ -6,17 +6,36 @@ const {
 } = require("./utils");
 
 
+// Main template and context
+const mainComponent = component(".");
+const sidebarRender = renderComponent(component("sidebar"));
+const mainTemplate = (content) =>
+	compileComponent(mainComponent)({
+		...mainComponent.context,
+		sidebar: sidebarRender,
+		content,
+	});
+
+// Style-specific helpers
+const mixinsSource = readFile("mixins.scss");
+const mixins = compile(mixinsSource)(mainComponent.context);
+const compileCss = sassToCss(mixins);
+const compileContentStyle = ({style, ...content}) =>
+	({style: compileCss(style), ...content});
+
+// Combine the two to get the full instantiation pattern
+const mainWithContent = (content) =>
+	mainTemplate(compileContentStyle(content));
+
+const mainWithComponent = (filename) =>
+	mainWithContent(renderComponent(component(filename)));
+
+
+// Build directory setup
 setupBuild();
 
-const indexComponent = component(".");
-const indexTemplate = compileComponent(indexComponent);
+// Rendering individual pages
+writeFile("index.html", mainWithComponent("home").markup);
 
-const mixinsSource = readFile("mixins.scss");
-const mixins = compile(mixinsSource)(indexComponent.context);
-const renderCss = sassToCss(mixins);
-
-const sidebar = renderComponent(component("./sidebar"));
-const index = indexTemplate({...indexComponent.context, sidebar});
-
-writeFile("index.html", index.markup);
-writeFile("style.css", renderCss(index.style));
+// Rendering global style
+writeFile("style.css", compileCss(mainTemplate({}).style));
