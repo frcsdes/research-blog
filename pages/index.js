7e42397c41461157768ba2c479b7533e44d65824
François Desrichard <git@desrichard.fr>
2019-01-02 15:42:26 +0100

    Layout and font tests

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,21 @@ const colors = {
 	grayBlack:  "#050505",
 };
 
+const fonts = {
+	list: [
+		{family: "Vidaloka", weight: 400, url: "fonts/Vidaloka-Regular.ttf"},
+		{family: "Raleway",  weight: 400, url: "fonts/Raleway-Regular.ttf"},
+		{family: "OpenSans", weight: 300, url: "fonts/OpenSans-Light.ttf"},
+		{family: "OpenSans", weight: 400, url: "fonts/OpenSans-Regular.ttf"},
+	],
+	body: "OpenSans",
+	display: "Vidaloka",
+};
+
 
 module.exports = {
 	title: "Research Blog",
 	breakpoints,
 	colors,
+	fonts,
 };
