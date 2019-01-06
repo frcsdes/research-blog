d2f78610561613d5fcefbbfe616f82bb8a44991b
François Desrichard <git@desrichard.fr>
2019-01-06 23:26:44 +0100

    Adding external links to the sidebar

--- pages/sidebar/index.js
+++ pages/sidebar/index.js
@@ -1,7 +1,34 @@
-module.exports = {
-	entries: [
-		{label: "Home", url: "index.html"},
-		{label: "Papers", url: "#"},
-		{label: "Posts", url: "#"},
-	],
-};
+const pages = [
+	{
+		label: "Home",
+		url: "index.html"
+	},
+	{
+		label: "Papers",
+		url: "#"
+	},
+	{
+		label: "Posts",
+		url: "#"
+	},
+];
+
+const links = [
+	{
+		label: "GitHub",
+		logo: "logo-github.svg",
+		url: "https://github.com/frcsdes",
+	},
+	{
+		label: "LinkedIn",
+		logo: "logo-linkedin.svg",
+		url: "https://www.linkedin.com/in/francois-desrichard",
+	},
+	{
+		label: "Medium",
+		logo: "logo-medium.svg",
+		url: "https://medium.com/@f.desrichard",
+	},
+];
+
+module.exports = {pages, links};
