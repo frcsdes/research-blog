3669f40a887c1daba4ba76ef4cb069de0737ce43
François Desrichard <git@desrichard.fr>
2020-10-18 18:26:28 +0200

    Remove cube from home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,17 +1,6 @@
 const hook = async (pb) => {
-	const cube = {
-		markup: pb.renderHb(await pb.readFile("cube.html")),
-		style: await pb.renderLess(await pb.readFile("cube.less")),
-	};
-
-	const markup = pb.renderHb(
-		await pb.readTemplate(),
-		pb.extendedContext({cube})
-	);
-
-	const less = pb.renderHb(await pb.readLess(), {cube});
-	const style = await pb.renderLess(less);
-
+	const markup = pb.renderHb(await pb.readTemplate());
+	const style = await pb.renderLess(await pb.readLess());
 	pb.writeFile("index.html", pb.renderPage({markup, style}));
 };
 
